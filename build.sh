f146e69e91b42cb9afbd7c29fadb598b11405946
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2022-07-11 00:52:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ set -ex
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
 
-g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I/home/alex/abledecoder -c FileChunk.cpp CommonChunk.cpp AbleChunk.cpp FormChunk.cpp SoundDataChunk.cpp FormatVersionChunk.cpp main.cpp
-g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I/home/alex/abledecoder -L/usr/local/opt/openssl/lib -lssl -lcrypto -o abledecoder FileChunk.o CommonChunk.o AbleChunk.o FormChunk.o SoundDataChunk.o FormatVersionChunk.o main.o
+g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I~/abledecoder -c FileChunk.cpp CommonChunk.cpp AbleChunk.cpp FormChunk.cpp SoundDataChunk.cpp FormatVersionChunk.cpp main.cpp
+g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I~/abledecoder -L/usr/local/opt/openssl/lib -o abledecoder FileChunk.o CommonChunk.o AbleChunk.o FormChunk.o SoundDataChunk.o FormatVersionChunk.o main.o -lssl -lcrypto
+
+
+
+#g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I~/abledecoder -L/usr/local/opt/openssl/lib -lssl -lcrypto -o abledecoder FileChunk.o CommonChunk.o AbleChunk.o FormChunk.o SoundDataChunk.o FormatVersionChunk.o main.o
 
 # If the compiler command fails, try this linking flags order
-#g++ -Wall -g -std=c++11 -I/usr/local/opt/openssl/include -I/home/alex/abledecoder -L/usr/local/opt/openssl/lib -o abledecoder FileChunk.o CommonChunk.o AbleChunk.o FormChunk.o SoundDataChunk.o FormatVersionChunk.o main.o -lssl -lcrypto
 
